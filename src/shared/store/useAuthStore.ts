6e92f8825e346f815157f89b948511ac5046b27f
NikhilRW <nikhilwankhede0707@gmail.com>
2025-10-07 17:48:10 +0530

    feat:Zustand Persistence Added

--- src/shared/store/useAuthStore.ts
+++ src/shared/store/useAuthStore.ts
@@ -1,25 +1,48 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage, StateStorage } from 'zustand/middleware';
 import { AuthState } from 'shared/types/Meeting';
+import { MMKV } from 'react-native-mmkv';
 
-export const useAuthStore = create<AuthState>((set, get) => ({
-  user: null,
-  isAuthenticated: false,
-  setUser: firebaseUser =>
-    set({
-      user: firebaseUser
-        ? {
-            uid: firebaseUser.uid,
-            email: firebaseUser.email,
-            displayName: firebaseUser.displayName,
-            photoURL: firebaseUser.photoURL,
-          }
-        : null,
-      isAuthenticated: !!firebaseUser,
-    }),
-  clearUser: () =>
-    set({
+const sessionStorage: StateStorage = {
+  getItem(name) {
+    const value = new MMKV().getString(name);
+    return value || '';
+  },
+  removeItem(name) {
+    new MMKV().delete(name);
+  },
+  setItem(name, value) {
+    new MMKV().set(name, value);
+  },
+};
+
+export const useAuthStore = create(
+  persist<AuthState>(
+    (set, get) => ({
       user: null,
       isAuthenticated: false,
+      setUser: firebaseUser =>
+        set({
+          user: firebaseUser
+            ? {
+                uid: firebaseUser.uid,
+                email: firebaseUser.email,
+                displayName: firebaseUser.displayName,
+                photoURL: firebaseUser.photoURL,
+              }
+            : null,
+          isAuthenticated: !!firebaseUser,
+        }),
+      clearUser: () =>
+        set({
+          user: null,
+          isAuthenticated: false,
+        }),
+      getUserEmail: () => get().user?.email || null,
     }),
-  getUserEmail: () => get().user?.email || null,
-}));
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
